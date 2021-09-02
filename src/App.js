02a8c8322e4d03e2efe915650b89458a403bc18b
Rocío Garrido <rocioganon@gmail.com>
2021-09-02 18:31:55 +0200

    add styles to main

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ const api = {
 };
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input className="search-bar" type="text" placeholder="Search..." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
