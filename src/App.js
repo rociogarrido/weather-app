18ced163b90ecea67da9bc0b8b3a57e4330ca968
Rocío Garrido <rocioganon@gmail.com>
2021-09-03 16:44:00 +0200

    add changing background image depending on the temp

--- src/App.js
+++ src/App.js
@@ -55,7 +55,15 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
           <input
