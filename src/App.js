e8e02a3a5c3d162e428d5dafaeb6e540e1834ed6
Rocío Garrido <rocioganon@gmail.com>
2021-09-03 15:59:31 +0200

    add styles to location-box and weather-box

--- src/App.js
+++ src/App.js
@@ -49,6 +49,10 @@ function App() {
           <div className="location">New York City, US</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
+        <div className="weather-box">
+          <div className="temp">15 °c</div>
+          <div className="weather">Sunny</div>
+        </div>
       </main>
     </div>
   );
